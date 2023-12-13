fa6ad534a096e67a0f6b93920521d07e93d540c6
upperdim <45048352+upperdim@users.noreply.github.com>
2023-12-13 09:20:26 +0100

    fix sort3, add sort4 and sort5

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 05:12:06 by tunsal            #+#    #+#             */
-/*   Updated: 2023/12/13 07:48:48 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/12/13 09:19:09 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,16 +101,62 @@ void	sort3(t_stack* s)
 	else if (largest_elem_idx == 2)
 	{
 		ra(s);
-		if (s->data[0] <= s->data[1])
+		if (s->data[2] > s->data[1])
 			sa(s);
 		return ;
 	}
 	else{
-		ft_printf("rra\n");
 		rra(s);
-		if (s->data[2] <= s->data[0])
+		if (s->data[2] > s->data[1])
 			sa(s);
 	}
+}
+
+void	sort4(t_stack* a, t_stack* b)
+{
+	int	smallest_idx;
+
+	smallest_idx = stack_get_smallest_elem_idx(a);
+	if (smallest_idx == 0)
+		rra(a);
+	else if (smallest_idx == 1)
+	{
+		ra(a);
+		ra(a);
+	}
+	else if (smallest_idx == 2)
+		ra(a);
+	if (stack_is_sorted_asc(a))
+		return ;
+	pb(a, b);
+	sort3(a);
+	pa(a, b);
+}
+
+void	sort5(t_stack *a, t_stack *b)
+{
+	int	smallest_idx;
+
+	smallest_idx = stack_get_smallest_elem_idx(a);
+	if (smallest_idx == 0)
+		rra(a);
+	else if (smallest_idx == 1)
+	{
+		rra(a);
+		rra(a);
+	}
+	else if (smallest_idx == 2)
+	{
+		ra(a);
+		ra(a);
+	}
+	else if (smallest_idx == 3)
+		ra(a);
+	if (stack_is_sorted_asc(a))
+		return ;
+	pb(a, b);
+	sort4(a, b);
+	pa(a, b);
 }
 
 int	main(int argc, char *argv[])
@@ -129,6 +175,10 @@ int	main(int argc, char *argv[])
 		sa(&a);
 	else if (a.top == 2)
 		sort3(&a);
+	else if (a.top == 3)
+		sort4(&a, &b);
+	else if (a.top == 4)
+		sort5(&a, &b);
 	else
 		mysort(&a, &b);
 
