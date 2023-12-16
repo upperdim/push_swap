3abd2b8df0d5dd0bbfc2ff6fd7fa7e6a96b1eee9
upperdim <45048352+upperdim@users.noreply.github.com>
2023-12-16 17:52:56 +0100

    update iteration to be more intiutive

--- utils/stack_move_elem_to_top.c
+++ utils/stack_move_elem_to_top.c
@@ -6,28 +6,48 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 10:58:51 by tunsal            #+#    #+#             */
-/*   Updated: 2023/12/16 10:59:01 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/12/16 14:51:29 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+static void	rotate(t_stack *s, char stack_name)
+{
+	if (stack_name == 'a')
+		ra(s);
+	else if (stack_name == 'b')
+		rb(s);
+	else
+		exit_error();
+}
+
+static void	reverse_rotate(t_stack *s, char stack_name)
+{
+	if (stack_name == 'a')
+		rra(s);
+	else if (stack_name == 'b')
+		rrb(s);
+	else
+		exit_error();
+}
+
 /* Move elem with `elem_idx` in stack `s` to the top. */
-void	stack_move_elem_to_top(t_stack *s, int elem_idx)
+void	stack_move_elem_to_top(t_stack *s, int elem_idx, char stack_name)
 {
 	int	i;
 
-	i = 0;
-	if (elem_idx > s-> top / 2)
-		while (i < s->top - elem_idx)
+	i = elem_idx;
+	if (elem_idx >= s-> top / 2)
+		while (i < s->top)
 		{
-			ra(s);
+			rotate(s, stack_name);
 			++i;
 		}
 	else
-		while (i <= elem_idx)
+		while (i >= 0)
 		{
-			rra(s);
-			++i;
+			reverse_rotate(s, stack_name);
+			--i;
 		}
 }
