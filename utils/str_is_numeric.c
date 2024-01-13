3916437515fbf2a16400741673e9b2ee796f9159
upperdim <45048352+upperdim@users.noreply.github.com>
2024-01-13 20:29:21 +0100

    fix uncovered cases after refactoring (unit tests would prevent this)

--- utils/str_is_numeric.c
+++ utils/str_is_numeric.c
@@ -6,11 +6,16 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 03:44:17 by tunsal            #+#    #+#             */
-/*   Updated: 2024/01/13 20:08:34 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/01/13 20:28:50 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+static int	char_is_numeric(char c)
+{
+	return (c >= '0' && c <= '9');
+}
 
 /* Return whether string only consists of digits or not. */
 int	str_is_numeric(char *s)
@@ -21,12 +26,17 @@ int	str_is_numeric(char *s)
 	len = ft_strlen(s);
 	if (s == NULL || len < 1)
 		return (0);
-	if (s[0] == '-' && len == 1)
+	if (s[0] == '-')
+	{
+		if (len == 1)
+			return (0);
+	}
+	else if (!char_is_numeric(s[0]))
 		return (0);
 	i = 1;
 	while (s[i] != '\0')
 	{
-		if (s[i] < '0' || s[i] > '9')
+		if (!char_is_numeric(s[i]))
 			return (0);
 		++i;
 	}
@@ -44,4 +54,5 @@ int	str_is_numeric(char *s)
 // 	printf("str_is_numeric(%s) = %s\n", "214748",      str_is_numeric("214748")      ? "true" : "false");
 // 	printf("str_is_numeric(%s) = %s\n", "2147483647",  str_is_numeric("2147483647")  ? "true" : "false");
 // 	printf("str_is_numeric(%s) = %s\n", "2147483648",  str_is_numeric("2147483648")  ? "true" : "false");
+// 	printf("str_is_numeric(%s) = %s\n", "a",           str_is_numeric("a")           ? "true" : "false");
 // }
